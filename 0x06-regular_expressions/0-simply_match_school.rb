6e3a7626d0549f3c1a4bd1a8e011a159a5fc387d
eziraa <ezratgab@gmail.com>
2023-05-04 04:24:26 +0300

    commited

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,21 +1,2 @@
 #!/usr/bin/env ruby
-#!/usr/bin/env ruby
-
-# check if an argument has been provided
-if ARGV.empty?
-  puts "Please provide an argument."
-  exit
-end
-
-# get the argument from the command line
-arg = ARGV[0]
-
-# define the regular expression pattern to match against
-regex = /\d{3}-\d{2}-\d{4}/
-
-# perform the regular expression match
-if arg.match(regex)
-  puts "Argument #{arg} matches the pattern."
-else
-  puts "Argument #{arg} does not match the pattern."
-end
+puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+puts ARGV[0].scan(/\[from:(\+?\w*)\]\s\[to:(\+?\w*)\]\s\[flags:(\S*)\]/).join(',')
